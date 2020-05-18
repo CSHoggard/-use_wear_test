a23f7d6cd3b2927e4ec26137d8f5548c4e8ec6db
Christian Hoggard <christianhoggard@gmail.com>
2020-05-18 09:44:54 +0100

    updating (importing data from GitHub directly)

--- Chan_et_al_2020.R
+++ Chan_et_al_2020.R
@@ -31,36 +31,41 @@
 if(!require("tidyverse")) install.packages('tidyverse', repos='http://cran.us.r-project.org') ### tidyverse 1.2.1
 if(!require("rms")) install.packages('rms', repos='http://cran.us.r-project.org') ### rms 5.1-3.1
 if(!require("ggpubr")) install.packages('ggpubr', repos='http://cran.us.r-project.org') ### ggpubr 0.2.3
+if(!require("RCurl")) install.packages('RCurl', repos='http://cran.us.r-project.org') ### readr 1.3.1
 
-dataset <- as.data.frame(readxl::read_excel("Chan_et_al_2020_1.xlsx", col_names = TRUE)) ### load the data frame
-compdata1 <- read.csv("Chan_et_al_2020_2.csv", header = T) ### load the comparative dataset
-compdata2 <- read.csv("Chan_et_al_2020_3.csv", header = T) ### load the comparative dataset (transformed)
+urlfile1 <- getURL("https://raw.githubusercontent.com/CSHoggard/-use_wear_test/master/dataset.csv")
+urlfile2 <- getURL("https://raw.githubusercontent.com/CSHoggard/-use_wear_test/master/comp_dataset_1.csv")
+urlfile3 <- getURL("https://raw.githubusercontent.com/CSHoggard/-use_wear_test/master/comp_dataset_2.csv")
 
-df1 <- select(dataset, "classification_id1" = UCT1, W)
-df2 <- select(dataset, "classification_id2" = UCT2, W)
-df3 <- select(dataset, "classification_id3" = UCT3, W)
+dataset <- read.csv(text = urlfile1)
+compdata1 <- read.csv(text = urlfile2)  
+compdata2 <- read.csv(text = urlfile3)
+
+df1 <- select(dataset, "classification_id1" = response_1, weight)
+df2 <- select(dataset, "classification_id2" = response_2, weight)
+df3 <- select(dataset, "classification_id3" = response_3, weight)
 
 df1$classification_id1 <- as.factor(df1$classification_id1)
 df2$classification_id2 <- as.factor(df2$classification_id2)
 df3$classification_id3 <- as.factor(df3$classification_id3)
 
-fig1a <- ggplot(df1, aes(classification_id1, W)) + geom_jitter(width = 0.15, size = 2, aes(colour = classification_id1)) + ylim(0,1) + labs(x = "Classification", y = "Weight (g)") + scale_colour_manual(values=c("#56B4E9", "#E69F00")) + theme_minimal() + theme(legend.position = "none")
-fig1b <- ggplot(df2, aes(classification_id2, W)) + geom_jitter(width = 0.15, size = 2, aes(colour = classification_id2)) + ylim(0,1) + labs(x = "Classification", y = "Weight (g)") + scale_colour_manual(values=c("#56B4E9", "#E69F00")) + theme_minimal() + theme(legend.position = "none")
-fig1c <- ggplot(df3, aes(classification_id3, W)) + geom_jitter(width = 0.15, size = 2, aes(colour = classification_id3)) + ylim(0,1) + labs(x = "Classification", y = "Weight (g)") + scale_colour_manual(values=c("#56B4E9", "#E69F00")) + theme_minimal() + theme(legend.position = "none")
+fig1a <- ggplot(df1, aes(classification_id1, weight)) + geom_jitter(width = 0.15, size = 2, aes(colour = classification_id1)) + ylim(0,1) + labs(x = "Classification", y = "Weight (g)") + scale_colour_manual(values=c("#56B4E9", "#E69F00")) + theme_minimal() + theme(legend.position = "none")
+fig1b <- ggplot(df2, aes(classification_id2, weight)) + geom_jitter(width = 0.15, size = 2, aes(colour = classification_id2)) + ylim(0,1) + labs(x = "Classification", y = "Weight (g)") + scale_colour_manual(values=c("#56B4E9", "#E69F00")) + theme_minimal() + theme(legend.position = "none")
+fig1c <- ggplot(df3, aes(classification_id3, weight)) + geom_jitter(width = 0.15, size = 2, aes(colour = classification_id3)) + ylim(0,1) + labs(x = "Classification", y = "Weight (g)") + scale_colour_manual(values=c("#56B4E9", "#E69F00")) + theme_minimal() + theme(legend.position = "none")
 ggarrange(fig1a, fig1b, fig1c, labels = c("B1", "B2", "B3"), ncol = 1, font.label = list(size = 7))
 ggsave("Figure_1.tiff", plot = last_plot(), width = 120, height = 160, units = "mm")
 
-glm1 <- glm(classification_id1 ~ W, df1, family = binomial) ### generalised linear model (binomial)
-glm2 <- glm(classification_id2 ~ W, df2, family = binomial) ### generalised linear model (binomial)
-glm3 <- glm(classification_id3 ~ W, df3, family = binomial) ### generalised linear model (binomial)
+glm1 <- glm(classification_id1 ~ weight, df1, family = binomial) ### generalised linear model (binomial)
+glm2 <- glm(classification_id2 ~ weight, df2, family = binomial) ### generalised linear model (binomial)
+glm3 <- glm(classification_id3 ~ weight, df3, family = binomial) ### generalised linear model (binomial)
 
 summary(glm1) ### summary (inc. parameter signifiance)
 summary(glm2) ### summary (inc. parameter signifiance)
 summary(glm3) ### summary (inc. parameter signifiance)
 
-fig2a <- ggplot(df1, aes(x=W, y=as.numeric(df1$classification_id1) - 1)) + geom_point(size = 2, aes(colour = classification_id1)) + stat_smooth(method="glm", se=TRUE, colour = "dark grey", method.args = list(family=binomial)) +  labs(x = "Weight (g)", y = "Classification") + scale_x_continuous(breaks=c(0,0.25,0.5,0.75)) + scale_y_continuous(breaks=c(0,1)) + theme_minimal() + theme(legend.position = "none") + scale_colour_manual(values=c("#56B4E9", "#E69F00"))
-fig2b <- ggplot(df2, aes(x=W, y=as.numeric(df2$classification_id2) - 1)) + geom_point(size = 2, aes(colour = classification_id2)) + stat_smooth(method="glm", se=TRUE, colour = "dark grey", method.args = list(family=binomial)) +  labs(x = "Weight (g)", y = "Classification") + scale_x_continuous(breaks=c(0,0.25,0.5,0.75)) + scale_y_continuous(breaks=c(0,1)) + theme_minimal() + theme(legend.position = "none") + scale_colour_manual(values=c("#56B4E9", "#E69F00"))
-fig2c <- ggplot(df3, aes(x=W, y=as.numeric(df3$classification_id3) - 1)) + geom_point(size = 2, aes(colour = classification_id3)) + stat_smooth(method="glm", se=TRUE, colour = "dark grey", method.args = list(family=binomial)) +  labs(x = "Weight (g)", y = "Classification") + scale_x_continuous(breaks=c(0,0.25,0.5,0.75)) + scale_y_continuous(breaks=c(0,1)) + theme_minimal() + theme(legend.position = "none") + scale_colour_manual(values=c("#56B4E9", "#E69F00"))
+fig2a <- ggplot(df1, aes(x=weight, y=as.numeric(classification_id1) - 1)) + geom_point(size = 2, aes(colour = classification_id1)) + stat_smooth(method="glm", se=TRUE, colour = "dark grey", method.args = list(family=binomial)) +  labs(x = "Weight (g)", y = "Classification") + scale_x_continuous(breaks=c(0,0.25,0.5,0.75)) + scale_y_continuous(breaks=c(0,1)) + theme_minimal() + theme(legend.position = "none") + scale_colour_manual(values=c("#56B4E9", "#E69F00"))
+fig2b <- ggplot(df2, aes(x=weight, y=as.numeric(classification_id2) - 1)) + geom_point(size = 2, aes(colour = classification_id2)) + stat_smooth(method="glm", se=TRUE, colour = "dark grey", method.args = list(family=binomial)) +  labs(x = "Weight (g)", y = "Classification") + scale_x_continuous(breaks=c(0,0.25,0.5,0.75)) + scale_y_continuous(breaks=c(0,1)) + theme_minimal() + theme(legend.position = "none") + scale_colour_manual(values=c("#56B4E9", "#E69F00"))
+fig2c <- ggplot(df3, aes(x=weight, y=as.numeric(classification_id3) - 1)) + geom_point(size = 2, aes(colour = classification_id3)) + stat_smooth(method="glm", se=TRUE, colour = "dark grey", method.args = list(family=binomial)) +  labs(x = "Weight (g)", y = "Classification") + scale_x_continuous(breaks=c(0,0.25,0.5,0.75)) + scale_y_continuous(breaks=c(0,1)) + theme_minimal() + theme(legend.position = "none") + scale_colour_manual(values=c("#56B4E9", "#E69F00"))
 figure_2 <-ggarrange(fig2a, fig2b, fig2c, labels = c("B1", "B2", "B3"), nrow = 2, ncol = 2, font.label = list(size = 8))
 annotate_figure(figure_2, bottom = text_grob("0: Correct  1: Incorrect", color = "black", face = "bold", size = 10))
 ggsave("Figure_2.tiff", width = 180, height = 180, units = "mm")
